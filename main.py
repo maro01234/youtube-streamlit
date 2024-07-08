a8a13d7893b9e51a8e111b4ae397ca415f38a0ea
maro01234 <machinehead7777777@gmail.com>
2024-07-08 19:09:37 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Strart!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+'Done!!'
